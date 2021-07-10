461a94d0c13c8c5e279addca6b949e956661f3ed
nulysses <adr.zivil@gmail.com>
2021-07-11 01:04:34 +0200

    1

---
+++ install.sh
@@ -0,0 +1,143 @@
+#!/usr/bin/env bash
+# ar18
+
+# Prepare script environment
+{
+  # Script template version 2021-07-11_00:01:52
+  script_dir_temp="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+  script_path_temp="${script_dir_temp}/$(basename "${0}")"
+  # Get old shell option values to restore later
+  if [ ! -v ar18_old_shopt_map ]; then
+    declare -A -g ar18_old_shopt_map
+  fi
+  shopt -s inherit_errexit
+  ar18_old_shopt_map["${script_path_temp}"]="$(shopt -op)"
+  set +x
+  # Set shell options for this script
+  set -e
+  set -E
+  set -o pipefail
+  set -o functrace
+}
+
+function clean_up() {
+  echo "cleanup ${ar18_parent_process}"
+  rm -rf "/tmp/${ar18_parent_process}"
+}
+trap clean_up SIGINT SIGHUP SIGQUIT SIGTERM
+
+function err_report() {
+  local path="${1}"
+  local lineno="${2}"
+  local msg="${3}"
+  clean_up
+  RED="\e[1m\e[31m"
+  NC="\e[0m" # No Color
+  printf "${RED}ERROR ${path}:${lineno}\n${msg}${NC}\n"
+}
+trap 'err_report "${BASH_SOURCE[0]}" ${LINENO} "${BASH_COMMAND}"' ERR
+
+{
+  # Make sure some modification to LD_PRELOAD will not alter the result or outcome in any way
+  if [ ! -v ar18_old_ld_preload_map ]; then
+    declare -A -g ar18_old_ld_preload_map
+  fi
+  if [ ! -v LD_PRELOAD ]; then
+    LD_PRELOAD=""
+  fi
+  ar18_old_ld_preload_map["${script_path_temp}"]="${LD_PRELOAD}"
+  LD_PRELOAD=""
+  # Save old script_dir variable
+  if [ ! -v ar18_old_script_dir_map ]; then
+    declare -A -g ar18_old_script_dir_map
+  fi
+  set +u
+  if [ ! -v script_dir ]; then
+    script_dir="${script_dir_temp}"
+  fi
+  ar18_old_script_dir_map["${script_path_temp}"]="${script_dir}"
+  set -u
+  # Save old script_path variable
+  if [ ! -v ar18_old_script_path_map ]; then
+    declare -A -g ar18_old_script_path_map
+  fi
+  set +u
+  if [ ! -v script_path ]; then
+    script_path="${script_path_temp}"
+  fi
+  ar18_old_script_path_map["${script_path_temp}"]="${script_path}"
+  set -u
+  # Determine the full path of the directory this script is in
+  script_dir="${script_dir_temp}"
+  script_path="${script_path_temp}"
+  #Set PS4 for easier debugging
+  export PS4='\e[35m${BASH_SOURCE[0]}:${LINENO}: \e[39m'
+  # Determine if this script was sourced or is the parent script
+  if [ ! -v ar18_sourced_map ]; then
+    declare -A -g ar18_sourced_map
+  fi
+  if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    ar18_sourced_map["${script_path_temp}"]=1
+  else
+    ar18_sourced_map["${script_path_temp}"]=0
+  fi
+  # Initialise exit code
+  if [ ! -v ar18_exit_map ]; then
+    declare -A -g ar18_exit_map
+  fi
+  ar18_exit_map["${script_path_temp}"]=0
+  # Save PWD
+  if [ ! -v ar18_pwd_map ]; then
+    declare -A -g ar18_pwd_map
+  fi
+  ar18_pwd_map["${script_path_temp}"]="${PWD}"
+  if [ ! -v ar18_parent_process ]; then
+    unset import_map
+    export ar18_parent_process="$$"
+  fi
+  # Get import module
+  if [ ! -v ar18_script_import ]; then
+    mkdir -p "/tmp/${ar18_parent_process}"
+    cd "/tmp/${ar18_parent_process}"
+    curl -O https://raw.githubusercontent.com/ar18-linux/ar18_lib_bash/master/ar18_lib_bash/script/import.sh >/dev/null 2>&1 && . "/tmp/${ar18_parent_process}/import.sh"
+    export ar18_script_import
+    cd "${ar18_pwd_map["${script_path_temp}"]}"
+  fi
+}
+#################################SCRIPT_START##################################
+
+ar18.script.import ar18.script.install
+
+. "${script_dir}/vars"
+
+ar18.script.install "${install_dir}" "${module_name}" "${script_dir}"
+
+##################################SCRIPT_END###################################
+set +x
+# Restore environment
+{
+  # Restore PWD
+  cd "${ar18_pwd_map["${script_path}"]}"
+  exit_script_path="${script_path}"
+  # Restore script_dir and script_path
+  script_dir="${ar18_old_script_dir_map["${exit_script_path}"]}"
+  script_path="${ar18_old_script_path_map["${exit_script_path}"]}"
+  # Restore LD_PRELOAD
+  LD_PRELOAD="${ar18_old_ld_preload_map["${exit_script_path}"]}"
+  # Restore old shell values
+  IFS=$'\n' shell_options=(echo ${ar18_old_shopt_map["${exit_script_path}"]})
+  for option in "${shell_options[@]}"; do
+    eval "${option}"
+  done
+}
+# Return or exit depending on whether the script was sourced or not
+{
+  if [ "${ar18_sourced_map["${exit_script_path}"]}" = "1" ]; then
+    return "${ar18_exit_map["${exit_script_path}"]}"
+  else
+    if [ "${ar18_parent_process}" = "$$" ]; then
+      clean_up
+    fi
+    exit "${ar18_exit_map["${exit_script_path}"]}"
+  fi
+}
